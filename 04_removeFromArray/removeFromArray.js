4b910b6b68d14e11bb2fe48c68f1e66047aca37c
Nishikino Maki <maki.keep@gmail.com>
2023-05-29 10:34:21 +0000

    Complete removeFromArray
    Not the most efficient solution.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  let output = array;
+  for (const arg of args) {
+    output = output.filter(el => el !== arg);
+  }
+  return output;
 };
 
 // Do not edit below this line
