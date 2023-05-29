d9cc9574040f5fa13f2a063e37ef69ca99723a8c
Nishikino Maki <maki.keep@gmail.com>
2023-05-29 09:30:46 +0000

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const chars = string.split('');
+  const reversedChars = chars.reverse();
+  const reversedString = reversedChars.join('');
+  return reversedString;
 
+  /* let reversedChars = [];
+  for (let i = chars.length - 1; i >= 0; i--) {
+    reversedChars.push(chars[i]);
+  }
+  return reversedChars.join(''); */
 };
 
 // Do not edit below this line
