985bffbe4d650b342e6f22b00fedb82fdb5fd4b3
Nishikino Maki <maki.keep@gmail.com>
2023-05-31 08:36:15 +0000

    Complete leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  const yearInt = parseInt(year);
+  const div4 = yearInt % 4 === 0;
+  const div100 = yearInt % 100 === 0;
+  const div400 = yearInt % 400 === 0;
+  return (div4 && (!div100 || div400));
 };
 
 // Do not edit below this line
