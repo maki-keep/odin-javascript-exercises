a34fb87b1852a3eb0108e054814c31139be29d86
Nishikino Maki <maki.keep@gmail.com>
2023-06-01 15:30:17 +0000

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  if (!Object.hasOwn(person, 'yearOfDeath')) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - person.yearOfBirth;
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+};
 
+const findTheOldest = function(people) {
+  // assume that the oldest person is the first, 
+  // then loop through people
+  let oldestPerson = people[0];
+  people.forEach(person => {
+    oldestPerson = getAge(person) > getAge(oldestPerson) ? person : oldestPerson;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
