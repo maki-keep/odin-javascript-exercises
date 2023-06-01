bed416f93aacb7e51cc9d8b61fed140c703e8e4d
Nishikino Maki <maki.keep@gmail.com>
2023-06-01 08:53:33 +0000

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(t) {
+  const result = (t - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(t) {
+  const result = (t * (9 / 5)) + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
