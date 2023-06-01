1f48eec689c4c6950450359817ecca955ef49709
Nishikino Maki <maki.keep@gmail.com>
2023-06-01 12:00:18 +0000

    Complete palindromes
    Not the most concise solution. Filters out underscores.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // filter out punctuation and word break, 
+  // anything but letters or digit
+  const cleanStr = str.toLowerCase().replace(/[^\w]|_/g, '');
 
+  let letters = cleanStr.split('');
+  let letters2 = [];
+  for (const ele of letters) {
+    letters2.unshift(ele);
+  }
+
+  // convert letters back into string, 
+  // without punctuation or word break
+  return letters.join('') === letters2.join('');
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.spec.js
+++ 09_palindromes/palindromes.spec.js
@@ -4,22 +4,40 @@ describe('palindromes', () => {
   test('works with single words', () => {
     expect(palindromes('racecar')).toBe(true);
   });
-  test.skip('works with punctuation ', () => {
+  test('works with single words', () => {
+    expect(palindromes('hello')).toBe(false);
+  });
+  test('works with punctuation ', () => {
     expect(palindromes('racecar!')).toBe(true);
   });
-  test.skip('works with upper-case letters ', () => {
+  test('works with punctuation ', () => {
+    expect(palindromes('hello!')).toBe(false);
+  });
+  test('works with upper-case letters ', () => {
     expect(palindromes('Racecar!')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
+  test('works with upper-case letters ', () => {
+    expect(palindromes('Hello!')).toBe(false);
+  });
+  test('works with multiple words', () => {
     expect(palindromes('A car, a man, a maraca.')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
+  test('works with multiple words', () => {
+    expect(palindromes('Hello, world!')).toBe(false);
+  });
+  test('works with multiple words', () => {
     expect(palindromes('Animal loots foliated detail of stool lamina.')).toBe(true);
   });
-  test.skip('doesn\'t just always return true', () => {
+  test('doesn\'t just always return true', () => {
     expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
   });
-  test.skip('works with numbers in a string', () => {
+  test('works with numbers in a string', () => {
     expect(palindromes('rac3e3car')).toBe(true);
+  });
+  test('works with underscores', () => {
+    expect(palindromes('__racecar_racecar__')).toBe(true);
+  });
+  test('works with underscores', () => {
+    expect(palindromes('__snake_case__')).toBe(false);
   });
 });
