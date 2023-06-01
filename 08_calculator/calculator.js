bb96f480fb5c69ffb3e4c197c131d791a873b600
Nishikino Maki <maki.keep@gmail.com>
2023-06-01 10:55:49 +0000

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(ele => {
+    sum += ele;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let res = 1;
+  arr.forEach(ele => {
+    res *= ele;
+  });
+  return res;
 };
 
-const power = function() {
-	
+const power = function(bas, pow) {
+  let res = 1;
+  for (let i = 0; i < pow; i++) {
+    res *= bas;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) {
+    return;
+  }
+  let fac = 1;
+  for (let i = num; i > 0; i--) {
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
